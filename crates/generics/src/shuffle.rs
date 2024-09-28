d52a45003600bdf03c3147f26c9d736b93569f80
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-09-28 21:33:50 +0300

    Better implement shuffle

--- crates/generics/src/shuffle.rs
+++ crates/generics/src/shuffle.rs
@@ -7,95 +7,33 @@ pub trait Shuffle<T> {
     fn shuffle(&mut self) -> Vec<T>;
 }
 
-impl Shuffle<u8> for Range<u8> {
-    fn shuffle(&mut self) -> Vec<u8> {
-        let mut vec: Vec<u8> = self.collect();
-        vec.shuffle(&mut thread_rng());
 
-        vec
-    }
+
+// Once Step is stable we can do the following
+// impl<T> Shuffle<T> for Range<T> where [T]: SliceRandom {
+//     fn shuffle(&mut self) -> Vec<T> {
+//         let mut vec: Vec<T> = self.collect();
+//         vec.shuffle(&mut thread_rng());
+//
+//         vec
+//     }
+// }
+
+macro_rules! shuffle_impl {
+    ($($t:ty)+) => ($(
+        impl Shuffle<$t> for Range<$t> {
+            fn shuffle(&mut self) -> Vec<$t> {
+                let mut vec: Vec<$t> = self.collect();
+                vec.shuffle(&mut thread_rng());
+
+                vec
+            }
+        }
+    )+)
 }
 
-impl Shuffle<i8> for Range<i8> {
-    fn shuffle(&mut self) -> Vec<i8> {
-        let mut vec: Vec<i8> = self.collect();
-        vec.shuffle(&mut thread_rng());
+shuffle_impl! { i8 u8 i16 u16 i32 u32 i64 u64 i128 u128 isize usize }
 
-        vec
-    }
-}
-
-impl Shuffle<u16> for Range<u16> {
-    fn shuffle(&mut self) -> Vec<u16> {
-        let mut vec: Vec<u16> = self.collect();
-        vec.shuffle(&mut thread_rng());
-
-        vec
-    }
-}
-
-impl Shuffle<i16> for Range<i16> {
-    fn shuffle(&mut self) -> Vec<i16> {
-        let mut vec: Vec<i16> = self.collect();
-        vec.shuffle(&mut thread_rng());
-
-        vec
-    }
-}
-
-impl Shuffle<u32> for Range<u32> {
-    fn shuffle(&mut self) -> Vec<u32> {
-        let mut vec: Vec<u32> = self.collect();
-        vec.shuffle(&mut thread_rng());
-
-        vec
-    }
-}
-
-impl Shuffle<i32> for Range<i32> {
-    fn shuffle(&mut self) -> Vec<i32> {
-        let mut vec: Vec<i32> = self.collect();
-        vec.shuffle(&mut thread_rng());
-
-        vec
-    }
-}
-
-impl Shuffle<u64> for Range<u64> {
-    fn shuffle(&mut self) -> Vec<u64> {
-        let mut vec: Vec<u64> = self.collect();
-        vec.shuffle(&mut thread_rng());
-
-        vec
-    }
-}
-
-impl Shuffle<i64> for Range<i64> {
-    fn shuffle(&mut self) -> Vec<i64> {
-        let mut vec: Vec<i64> = self.collect();
-        vec.shuffle(&mut thread_rng());
-
-        vec
-    }
-}
-
-impl Shuffle<u128> for Range<u128> {
-    fn shuffle(&mut self) -> Vec<u128> {
-        let mut vec: Vec<u128> = self.collect();
-        vec.shuffle(&mut thread_rng());
-
-        vec
-    }
-}
-
-impl Shuffle<i128> for Range<i128> {
-    fn shuffle(&mut self) -> Vec<i128> {
-        let mut vec: Vec<i128> = self.collect();
-        vec.shuffle(&mut thread_rng());
-
-        vec
-    }
-}
 
 #[cfg(test)]
 mod tests {
