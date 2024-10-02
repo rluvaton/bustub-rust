093fbfed484f09e350d239154282f19426b66ad5
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-10-02 16:17:04 +0300

    add 2024 buffer pool trait

--- crates/db_core/src/buffer/mod.rs
+++ crates/db_core/src/buffer/mod.rs
@@ -1,5 +1,6 @@
 mod buffer_pool_manager;
 mod replacer;
+mod buffer_pool_manager_2;
 
 pub use buffer_pool_manager::{BufferPoolManager, BufferPoolManagerStats, PinPageGuard, PinReadPageGuard, PinWritePageGuard, PageLockComparator, AlwaysValidPageLockComparator, errors};
 pub use replacer::*;
