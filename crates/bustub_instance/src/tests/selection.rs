3909beb883a68ec47671655dd14071d948c6d86b
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-10-18 13:33:48 +0300

    add tests for selection

--- crates/bustub_instance/src/tests/selection.rs
+++ crates/bustub_instance/src/tests/selection.rs
@@ -22,4 +22,20 @@ mod tests {
 
         assert_eq!(actual, expected)
     }
+
+    #[test]
+    fn should_select_with_column_constant_filter() {
+        let mut instance = BustubInstance::in_memory(None);
+        instance.generate_mock_table();
+
+        let sql = format!("SELECT number from {} where number = 1", MockTableName::Table123);
+
+        let actual = instance.execute_single_select_sql(sql.as_str(), CheckOptions::default()).expect("Should execute");
+
+        let expected = actual.create_with_same_schema(vec![
+            vec![Value::from(1)],
+        ]);
+
+        assert_eq!(actual, expected)
+    }
 }
