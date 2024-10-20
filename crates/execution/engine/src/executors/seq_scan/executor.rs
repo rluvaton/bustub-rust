e00b378d5539ebb1dac811220bc91471c50cd552
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-10-20 12:25:00 +0300

    start add seq-scan exec

---
+++ crates/execution/engine/src/executors/seq_scan/executor.rs
@@ -0,0 +1,80 @@
+use crate::context::ExecutorContext;
+use crate::executors::{Executor, ExecutorImpl, ExecutorItem, ExecutorMetadata};
+use catalog_schema::Schema;
+use catalog_schema_mocks::{MockDataIterator, MockTableName};
+use planner::{PlanNode, PlanType};
+use std::fmt;
+use std::fmt::Debug;
+use std::ops::Deref;
+use std::sync::Arc;
+use tuple::Tuple;
+
+#[must_use = "iterators are lazy and do nothing unless consumed"]
+pub struct SeqScanExecutor<'a> {
+    /// The executor context in which the executor runs
+    ctx: Arc<ExecutorContext<'a>>,
+
+    // ----
+
+    /** The plan node for the scan */
+    plan: &'a PlanType,
+
+    /** The cursor for the current mock scan */
+
+    iter: MockDataIterator,
+}
+
+impl<'a> SeqScanExecutor<'a> {
+    pub(crate) fn new(plan: &'a PlanType, ctx: Arc<ExecutorContext<'a>>) -> Self {
+        let mock_table_name: MockTableName = match &*plan {
+            PlanType::MockScan(s) => s.get_table().as_str().try_into().expect("Must be a valid mock table name"),
+            _ => unreachable!(),
+        };
+
+        Self {
+            plan,
+            ctx,
+            iter: mock_table_name.get_data_iter(),
+        }
+    }
+}
+
+impl Debug for SeqScanExecutor<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SeqScan").finish()
+    }
+}
+
+
+impl Iterator for SeqScanExecutor<'_>
+{
+    type Item = ExecutorItem;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        let (values, rid) = self.iter.next()?;
+
+        Some((
+            Tuple::from_value(values, self.plan.get_output_schema().deref()),
+            rid
+        ))
+    }
+}
+
+impl ExecutorMetadata for SeqScanExecutor<'_> {
+    fn get_output_schema(&self) -> Arc<Schema> {
+        self.plan.get_output_schema()
+    }
+
+    fn get_context(&self) -> &ExecutorContext {
+        &self.ctx
+    }
+}
+
+impl<'a> Into<ExecutorImpl<'a>> for SeqScanExecutor<'a> {
+    fn into(self) -> ExecutorImpl<'a> {
+        ExecutorImpl::SeqScan(self)
+    }
+}
+
+impl<'a> Executor<'a> for SeqScanExecutor<'a> {}
